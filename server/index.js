1109e31b563312b5bebd31661a306f30ea9ae304
AnanyaJindal1145 <ajindal1145@gmail.com>
2023-11-14 02:16:49 +0530

    added node modules,folders,started html, js files

---
+++ server/index.js
@@ -0,0 +1,5 @@
+//installed yup for validation,monk for mongo db(to be replaced by mongoose),helmet(security),morgan(for logs),cors,dotenv,nodemon
+const express= require('express');
+const cors= require('cors');
+const morgan= require('morgan');
+const helmet= require('helmet');
